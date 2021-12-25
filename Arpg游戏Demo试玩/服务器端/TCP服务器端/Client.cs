f268c3147234dfe8af7a30f2d7891d5b8bedd35b
goodxiaozhou <838835185@qq.com>
2021-12-26 01:01:04 +0800

    a Arpg Demo

---
+++ Arpg游戏Demo试玩/服务器端/TCP服务器端/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP服务器端
+{
+    class Client
+    {
+        private Socket clientScoket;
+        private Thread t;
+        private byte[] data = new byte[1024];
+        public Client(Socket s)
+        {
+            clientScoket = s;
+            t = new Thread(ReceiveMessage);
+            t.Start();
+        }
+        private void ReceiveMessage()
+        {
+            //循环接收数据
+            while (true)
+            {
+                //判断socket连接是否断开
+                if (clientScoket.Poll(10, SelectMode.SelectRead))
+                {
+                    if (clientScoket!=null)
+                    {
+                        clientScoket.Close();
+                    }
+                    
+                    Console.WriteLine("断开连接");
+                    break;//连接断开结束线程
+                }
+                int length = clientScoket.Receive(data);
+                string message = Encoding.UTF8.GetString(data, 0, length);
+                //接受数据，把数据分发到客户端.....
+                Program.NroadCastMessage(message );
+                Console.WriteLine("收到信息：" + message);
+            }
+        }
+        public void Sendmessage(string message)
+        {
+            if (message!=""&& message!=null)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                clientScoket.Send(data);
+            }
+        }
+        public bool Connected
+        {
+            get
+                { return clientScoket.Connected; }
+        }
+
+    }
+}
---
+++ Arpg游戏Demo试玩/服务器端/TCP服务器端/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP服务器端
+{
+    class Program
+    {
+        static List<Client> clientList = new List<Client>();
+        /// <summary>
+        /// 广播消息
+        /// </summary>
+        /// <param name="message"></param>
+        public static void NroadCastMessage(string message)
+        {
+            //创建没有连接的集合
+            if (message!=null&& message!="")
+            {
+                var notConnectedList = new List<Client>();
+                for (int i = 0; i < clientList.Count; i++)
+                {
+                    if (clientList[i].Connected)
+                    {
+
+                        clientList[i].Sendmessage(message);
+
+
+                    }
+                    else
+                    {
+                        notConnectedList.Add(clientList[i]);
+                    }
+                    for (int j = 0; j < notConnectedList.Count; j++)
+                    {
+                        clientList.Remove(notConnectedList[j]);
+                    }
+
+                }
+            }
+            
+            //foreach (var item in clientList)
+            //{
+            //    if (item.Connected)
+            //    {
+            //        item.Sendmessage(message);
+            //    }
+            //    else
+            //    {
+            //        notConnectedList.Add(item);
+            //    }
+            //    foreach (var temp in notConnectedList)
+            //    {
+            //        clientList.Remove(temp);
+            //    }
+                
+            //}         
+
+        }
+        static void Main(string[] args)
+        {
+             
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork, 
+                SocketType.Stream, ProtocolType.Tcp);
+            //tcpServer.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.6"),5566));
+            IPAddress iPAddress = IPAddress.Parse("172.21.68.121");
+            EndPoint point = new IPEndPoint(iPAddress, 7788);
+            tcpServer.Bind(point);
+            tcpServer.Listen(100 );
+            Console.WriteLine("连接中");
+           
+            while (true)
+            {
+                Socket clientSocket = tcpServer.Accept();
+                Console.WriteLine("连接成功");
+                Client client = new Client(clientSocket);//客服端通信逻辑放到client类里面进行处理
+                clientList.Add(client);
+                //Console.ReadKey();
+            }
+            
+           
+        }
+    }
+}
